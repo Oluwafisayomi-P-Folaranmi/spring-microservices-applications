45b35cfa26e568b268114f279118867d7d17e4bf
Oluwafisayomi Folaranmi <folaranmifisayomi@gmail.com>
2025-02-08 01:21:48 +0100

    initial commit

---
+++ 0x02-basic-order-application/z-Projects/basic-order/src/test/java/com/opf/basic_order/BasicOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opf.basic_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-microservices-applications
-spring-microservices-applications
+
+This is my documentation on what one can do with Microservices using the Spring Framework.
+
+&copy; 2025 Oluwafisayomi Folaranmi. All rights reserved.
